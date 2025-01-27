d2617deb872775f6dd99a4ccb361a4969a761cc1
giorgiospirostopala <gio.topala@gmail.com>
2025-01-27 22:05:13 +0100

    add: snack 2

--- script.js
+++ script.js
@@ -18,26 +18,14 @@ let biciPesoMinore = biciclette[0];
 for (let i = 1; i < biciclette.length; i++) {
 
     if (biciclette[i].peso < biciPesoMinore.peso) {
+
         biciPesoMinore = biciclette[i];
+        
     }
 
 }
 
 console.log(`La bici che pesa di meno è la ${biciPesoMinore.nome} con un peso di ${biciPesoMinore.peso}.`);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 //?     Snack2
@@ -46,3 +34,41 @@ console.log(`La bici che pesa di meno è la ${biciPesoMinore.nome} con un peso d
 //-       3.  Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
 //-        4.  Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
 //-         5.  Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+
+// funzione per numeri randomici
+function generaNumeroRandom(min, max) {
+
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+
+}
+
+// creo array
+const squadre = [
+
+    { nome: "Juventus", puntiFatti: 0, falliSubiti: 0 },
+    { nome: "Milan", puntiFatti: 0, falliSubiti: 0 },
+    { nome: "Inter", puntiFatti: 0, falliSubiti: 0 },
+    { nome: "Roma", puntiFatti: 0, falliSubiti: 0 },
+    { nome: "Napoli", puntiFatti: 0, falliSubiti: 0 }
+
+];
+
+// ciclo for per assegnare i nuovi valori (numeri random) alle chiavi "puntiFatti"/"falliSubiti"
+for (let i = 0; i < squadre.length; i++) {
+
+    squadre[i].puntiFatti = generaNumeroRandom(0, 100);
+    squadre[i].falliSubiti = generaNumeroRandom(0, 100);
+    console.log(squadre[i]);
+}
+
+// nuovo array vuoto per pusharci dentro i nuovi elementi coi soli valori "nome" e "falliSubiti"
+const soloNomiEFalli = [];
+
+for (let i = 0; i < squadre.length; i++) {
+
+    soloNomiEFalli.push({nome: squadre[i]["nome"], falliSubiti: squadre[i]["falliSubiti"]});
+    console.log(soloNomiEFalli[i]);
+
+}
+
+// console.log(squadre);
